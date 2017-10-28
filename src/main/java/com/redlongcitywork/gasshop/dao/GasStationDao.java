a7d2719b1494b08d21d650349b45353bda12f685
Vladimir_Kharchenko <RedLongCity@gmail.com>
2017-10-28 21:56:12 +0300

    Added GasStationDao and GasStationDaoImpl

---
+++ src/main/java/com/redlongcitywork/gasshop/dao/GasStationDao.java
@@ -0,0 +1,23 @@
+package com.redlongcitywork.gasshop.dao;
+
+import com.redlongcitywork.gasshop.models.GasStation;
+import java.util.List;
+
+/**
+ *
+ * @author redlongcity 
+ * 28/10/2017
+ */
+public interface GasStationDao {
+
+    List<GasStation> findAll();
+
+    GasStation findById(Integer id);
+
+    void save(GasStation station);
+
+    void delete(GasStation station);
+
+    void update(GasStation station);
+
+}
--- src/main/java/com/redlongcitywork/gasshop/dao/GasStationImpl.java
+++ src/main/java/com/redlongcitywork/gasshop/dao/GasStationImpl.java
@@ -3,7 +3,7 @@ package com.redlongcitywork.gasshop.dao;
 import static com.google.common.base.Preconditions.checkNotNull;
 import com.redlongcitywork.gasshop.jdbcutils.ConnectionProvider;
 import com.redlongcitywork.gasshop.jdbcutils.Transaction;
-import com.redlongcitywork.gasshop.models.Fuel;
+import com.redlongcitywork.gasshop.models.GasStation;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -19,19 +19,19 @@ import org.springframework.stereotype.Repository;
  *
  * @author redlongcity 28/10/2017
  */
-@Repository("fuelDao")
-public class FuelDaoImpl implements FuelDao {
+@Repository("gasStationDao")
+public class GasStationImpl implements GasStationDao {
 
     @Autowired
     Transaction tx;
 
-    private static final Logger LOG = Logger.getLogger(FuelDaoImpl.class.getName());
+    private static final Logger LOG = Logger.getLogger(GasStationImpl.class.getName());
 
     @Override
-    public List<Fuel> findAll() {
-        List<Fuel> list = new LinkedList<Fuel>();
+    public List<GasStation> findAll() {
+        List<GasStation> list = new LinkedList<GasStation>();
         try {
-            String query = "select * from fuels";
+            String query = "select * from gas_stations";
             PreparedStatement statement = ConnectionProvider.getInstance().
                     getConnection().prepareStatement(query);
             ResultSet set = statement.executeQuery();
@@ -45,64 +45,39 @@ public class FuelDaoImpl implements FuelDao {
     }
 
     @Override
-    public Fuel findById(Integer id) {
+    public GasStation findById(Integer id) {
         checkNotNull(id);
 
-        Fuel fuel = null;
+        GasStation station = null;
         try {
-            Connection connection = ConnectionProvider.getInstance().getConnection();
-            String query = "select from fuels where id= ?";
+            Connection connection = ConnectionProvider.getInstance().
+                    getConnection();
+            String query = "select from fuels where id = ?";
             PreparedStatement statement = connection.prepareStatement(query);
             statement.setInt(1, id);
             ResultSet set = statement.executeQuery();
             if (set.next()) {
-                fuel = convert(set);
+                station = convert(set);
             }
         } catch (SQLException e) {
             LOG.log(Level.WARNING, e.getMessage());
         }
-        return fuel;
+        return station;
     }
 
     @Override
-    public void save(Fuel fuel) {
+    public void save(GasStation station) {
         Connection connection = tx.getConnection();
 
         try {
             tx.begin();
 
-            String query = "insert into fuels (id, name) values (?, ?)";
+            String query = "insert into gas_sations (id, name) values(?, ?)";
             PreparedStatement statement = connection.prepareStatement(query);
-            statement.setInt(1, fuel.getId());
-            statement.setString(2, fuel.getName());
+            statement.setInt(1, station.getId());
+            statement.setString(2, station.getName());
 
             statement.executeUpdate();
-
-            tx.commit();
-
-        } catch (SQLException e) {
-            LOG.log(Level.WARNING, e.getMessage());
-        } finally {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                LOG.log(Level.WARNING, e.getMessage());
-            }
-        }
-    }
-
-    @Override
-    public void delete(Fuel fuel) {
-        Connection connection = tx.getConnection();
-
-        try {
-            tx.begin();
-
-            String query = "delete from fuels where id = ?";
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setInt(1, fuel.getId());
-            statement.executeUpdate();
-
             tx.commit();
         } catch (SQLException e) {
             LOG.log(Level.WARNING, e.getMessage());
@@ -116,30 +91,53 @@ public class FuelDaoImpl implements FuelDao {
     }
 
     @Override
-    public void update(Fuel fuel) {
-        try {
-            String query = "update fuels set name = ? where id= ?";
+    public void delete(GasStation station) {
+        Connection connection = tx.getConnection();
 
+        try {
+            tx.begin();
+
+            String query = "delete from gas_stations where id= ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, station.getId());
+            statement.executeUpdate();
+
+            tx.commit();
+        } catch (SQLException e) {
+            LOG.log(Level.WARNING, e.getMessage());
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                LOG.log(Level.WARNING, e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void update(GasStation station) {
+        try {
+            String query = "update gas_stations set name= ? where id = ?";
             PreparedStatement statement = tx.getConnection().
                     prepareStatement(query);
-            statement.setString(1, fuel.getName());
-            statement.setInt(2, fuel.getId());
+            statement.setString(1, station.getName());
+            statement.setInt(2, station.getId());
             statement.executeUpdate();
         } catch (SQLException e) {
             LOG.log(Level.WARNING, e.getMessage());
         }
     }
 
-    private Fuel convert(ResultSet resultSet) {
-        Fuel fuel = new Fuel();
+    private GasStation convert(ResultSet set) {
+        GasStation station = new GasStation();
 
         try {
-            fuel.setId(resultSet.getInt("id"));
-            fuel.setName(resultSet.getString("name"));
+            station.setId(set.getInt("id"));
+            station.setName(set.getString("name"));
         } catch (SQLException e) {
             LOG.log(Level.WARNING, e.getMessage());
         }
-        return fuel;
+        return station;
     }
 
 }
